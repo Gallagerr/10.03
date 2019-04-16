4c5610b06e71e1b825f53ae0b71e9c0ed015b436
sadiev <sadiev@itstep.kz>
2019-04-16 20:39:43 +0600

    Добавьте файлы проекта.

---
+++ 10.03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._03
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Fibbonachy fibbonachy = new Fibbonachy();
+      fibbonachy.FibbonachyInText();
+    }
+  }
+}
